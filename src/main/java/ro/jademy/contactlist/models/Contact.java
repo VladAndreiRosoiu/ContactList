376da324d973ea0532cdaccfc72dcacc089d3d3d
Alex Zaharioiu <alexzaharioiu7@gmail.com>
2020-09-04 22:44:55 +0300

    Initial commit

--- src/main/java/ro/jademy/contactlist/models/Contact.java
+++ src/main/java/ro/jademy/contactlist/models/Contact.java
@@ -1,23 +1,50 @@
 package ro.jademy.contactlist.models;
 
-public class Contact {
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.Date;
+
+public class Contact implements Comparable<Contact> {
+
+    private int id;                                            // for internal operations only (not being displayed)
+    //private String title;                                    // ex: Mr., Mrs., Ms., Miss, Prof., Dr., Eng.;
     private String firstName;
     private String lastName;
     private String email;
     private Company company;
-    //private Group contactGroup;
-    private int id;
+    private PhoneNumber phoneNumber;
+    private Group group;
+    private Address address;
+    private Date birthDate;
 
-//    public Contact() {
-//    }
-
-    public Contact(String firstName, String lastName, int id) {
+    public Contact(int id,  String firstName, String lastName, String email, Company company,
+                   PhoneNumber phoneNumber, Group group, Address address, Date birthDate) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.id=id;
-        //this.contactGroup = Group.MY_CONTACTS;
+        this.email = email;
+        this.company = company;
+        this.phoneNumber = phoneNumber;
+        this.group = group;
+        this.address = address;
+        this.birthDate = birthDate;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+  /*  public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }*/
 
     public String getFirstName() {
         return firstName;
@@ -51,12 +78,49 @@ public class Contact {
         this.company = company;
     }
 
-    public int getId() {
-        return id;
+    public PhoneNumber getPhoneNumber() {
+        return phoneNumber;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setPhoneNumber(PhoneNumber phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return  StringUtils.center(firstName, 15, " ") +
+                StringUtils.center(lastName, 15, " ") +
+                StringUtils.center(phoneNumber.getCountryCode(), 1, " ") +
+                StringUtils.center(String.valueOf(phoneNumber.getPhoneNumber()), 15, " ");
+    }
+
+
+    @Override
+    public int compareTo(Contact otherContact) {
+       return this.firstName.compareTo(otherContact.firstName);
+    }
 }
--- src/main/java/ro/jademy/contactlist/models/PhoneBook.java
+++ src/main/java/ro/jademy/contactlist/models/PhoneBook.java
@@ -1,14 +1,25 @@
 package ro.jademy.contactlist.models;
 
-import java.util.Map;
 
 public class PhoneBook {
 
-    private Map<PhoneNumber, Contact> phoneBookMap;
+    // Phone Book MAIN MENU
+    // 1. Display contacts (firstName, lastName, phoneNumber)
+    // 2. Select contact (By index (A-Z) -> create a subset of first letter of the contact, and after that by firstName)
+            // Contact Menu
+            // 1. Edit contact         -----> sub menu To edit all attributes (Return to Contact Menu)
+            // 2. Remove contact
+            // 3. Add to black list
+            // 4. Add to Favorites
+            // 5. Return to Phone Book
+    // 3. Search by
+         // 1. firstName
+         // 2. lastName
+         // 3. phoneNumber
+         // 4. Return to Phone Book
+    // 4. Add new Contact
+    // 5. Exit App
 
-    public PhoneBook(Map<PhoneNumber,Contact> phoneBookMap){
-        this.phoneBookMap=phoneBookMap;
-    }
 
 
 }
